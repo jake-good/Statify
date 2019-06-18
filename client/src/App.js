38eb9eeb8f88577ef6e5433d59c2089116ecb8f9
jake-good <jgoo479@aucklanduni.ac.nz>
2019-06-18 22:30:32 +1200

    initial

--- client/src/App.js
+++ client/src/App.js
@@ -1,86 +1,110 @@
-import React, { Component } from 'react';
-import './App.css';
-import Artist from './Artist';
+import React, { Component } from "react";
+import "./App.css";
+import Artist from "./Artist";
 
-import SpotifyWebApi from 'spotify-web-api-js';
+import SpotifyWebApi from "spotify-web-api-js";
 const spotifyApi = new SpotifyWebApi();
 
 class App extends Component {
-
-  constructor(){
+  constructor() {
     super();
     const params = this.getHashParams();
     const token = params.access_token;
     if (token) {
-      console.log('got token')
+      console.log("got token");
       spotifyApi.setAccessToken(token);
     }
     this.state = {
       loggedIn: token ? true : false,
-      nowPlaying: { name: 'Not Checked', albumArt: '' },
+      nowPlaying: { name: "Not Checked", albumArt: "" },
       topArtists: []
-    }
+    };
   }
-
 
   getHashParams() {
     var hashParams = {};
-    var e, r = /([^&;=]+)=?([^&;]*)/g,
-        q = window.location.hash.substring(1);
-    e = r.exec(q)
-    while (e) {
-       hashParams[e[1]] = decodeURIComponent(e[2]);
-       e = r.exec(q);
+    var e,
+      r = /([^&;=]+)=?([^&;]*)/g,
+      q = window.location.hash.substring(1);
+    while ((e = r.exec(q))) {
+      hashParams[e[1]] = decodeURIComponent(e[2]);
     }
-    console.log('got params');
     return hashParams;
   }
 
-
   getData() {
-    if(this.state.loggedIn) {
-    spotifyApi.getMyTopArtists({time_range: 'long_term'}).then
-    (response => 
-      this.setState({topArtists: response.items})
-    );
-    this.getArtistPictures();
-  }}
+    if (this.state.loggedIn) {
+      spotifyApi
+        .getMyTopArtists({ time_range: "long_term" })
+        .then(response => this.setState({ topArtists: response.items }));
+      //this.getArtistPictures();
+    }
+  }
 
   getArtistPictures() {
-    let images = ''
-    if(this.state.loggedIn) {
+    let images = "";
+    if (this.state.loggedIn) {
       let artistArray = this.state.topArtists;
       let artistImageURLs = [];
       for (let i = 0; i < artistArray.length; i++) {
         artistImageURLs.push(artistArray[i].images[0].url);
       }
-      images = artistImageURLs.map(url => <img src={url}></img>);
-      console.log(artistImageURLs);
+      images = artistImageURLs.map(url => <img src={url} />);
     }
     return images;
   }
 
   makeArtists() {
-    let Artists = this.state.topArtists.map(item => <Artist artist={item}></Artist>);
+    let Artists = this.state.topArtists.map(item => <Artist artist={item} />);
     return Artists;
   }
-  
-  render() {
-    let logInButton;
-    if (!this.state.loggedIn) {
-      logInButton = <button type="button">Click me</button>
-    } else {
-      logInButton = '';
+
+  generateRandomString(length) {
+    var text = "";
+    var possible =
+      "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    for (var i = 0; i < length; i++) {
+      text += possible.charAt(Math.floor(Math.random() * possible.length));
     }
+    return text;
+  }
+
+  redirect() {
+    var client_id = "d8c9e8ca3c784898bdf939f51ff6136f"; // Your client id
+    var redirect_uri = "http://localhost:3000"; // Your redirect uri
+    var state = this.generateRandomString(16);
+    var stateKey = "spotify_auth_state";
+    localStorage.setItem(stateKey, state);
+    var scope = "user-read-private user-read-email";
+    var url = "https://accounts.spotify.com/authorize";
+    url += "?response_type=token";
+    url += "&client_id=" + encodeURIComponent(client_id);
+    url += "&scope=" + encodeURIComponent(scope);
+    url += "&redirect_uri=" + encodeURIComponent(redirect_uri);
+    url += "&state=" + encodeURIComponent(state);
+    window.location = url;
+  }
+
+  render() {
+    let Container;
+    if (!this.state.loggedIn) {
+      Container = (
+        <button type="button" onClick={() => this.redirect()}>
+          View my top artists
+        </button>
+      );
+    } else {
+      this.getData();
+      Container = this.makeArtists();
+    }
+
     return (
       <div className="App">
-      <a href="http://localhost:8888">
-        {logInButton}
-      </a>
-      <button onClick={() => this.getData()}>Get top artists</button>
-      <div className="imgBox">
-        {this.makeArtists()}
-      </div>
+        <header>
+          <h1 id="main_title">Statify</h1>
+          <h2 id="sub_title">View your top spotify artists!</h2>
+        </header>
+        {Container}
       </div>
     );
   }
--- client/src/Artist.js
+++ client/src/Artist.js
@@ -1,22 +1,60 @@
-import React from 'react'
-
+import React from "react";
 
 class Artist extends React.Component {
-    constructor() {
-        super();
-    }
+  constructor() {
+    super();
+    this.state = {
+      expanded: false
+    };
+  }
 
-    render() {
-        return (
-            //In this div display the artist picture, name, popularity and have it link to their spotify page
-            <div className='Artist'>
-                <h1>{this.props.artist.name}</h1>
-                <p>popularity: {this.props.artist.popularity}</p>
-                <p><a href={this.props.artist.external_urls.spotify}>link to the artist page</a></p>
-                <img src={this.props.artist.images[0].url}></img>
-            </div>
-        )
+  toggle() {
+    this.setState({
+      expanded: !this.state.expanded
+    });
+    console.log("toggle");
+  }
+
+  render() {
+    let container;
+    if (!this.state.expanded) {
+      container = (
+        <div className="unexpanded_artist_title">
+          <p>{this.props.artist.name}</p>
+        </div>
+      );
+    } else {
+      let genresList = "";
+      for (var genre in this.props.artist.genres) {
+        genresList += this.props.artist.genres[genre] + ", ";
+      }
+      container = (
+        <div className="expanded_artist">
+          <section className="img_box">
+            <img src={this.props.artist.images[0].url} />
+          </section>
+          <div className="artist_details">
+            <h1 id="expanded_title">{this.props.artist.name}</h1>
+            <p id="expand_details">
+              Number of followers: {this.props.artist.followers.total}
+            </p>
+            <p id="expand_details">Genres: {genresList}</p>
+            <p id="expand_details">
+              <a href={this.props.artist.external_urls.spotify}>
+                <span>View the artists Spotify page</span>
+              </a>
+            </p>
+          </div>
+        </div>
+      );
     }
+    return (
+      //In this div display the artist picture, name, popularity and have it link to their spotify page
+      <div className="Artist" onClick={() => this.toggle()}>
+        {container}
+      </div>
+    );
+  }
 }
 
-export default Artist
+export default Artist;
