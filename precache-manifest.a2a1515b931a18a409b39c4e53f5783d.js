1210a770e720ef8cf8aa13edbc9302c6e57b92fa
jake-good <jgoo479@aucklanduni.ac.nz>
2019-06-18 22:47:00 +1200

    Updates

---
+++ precache-manifest.a2a1515b931a18a409b39c4e53f5783d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "87e208e86ae124bc5f6e",
+    "url": "/Statify/static/js/runtime~main.73e1833e.js"
+  },
+  {
+    "revision": "b8fcbfd8bba7b67c2839",
+    "url": "/Statify/static/js/main.b4084880.chunk.js"
+  },
+  {
+    "revision": "9cf193abacea20dda3d9",
+    "url": "/Statify/static/js/2.60e68fdf.chunk.js"
+  },
+  {
+    "revision": "b8fcbfd8bba7b67c2839",
+    "url": "/Statify/static/css/main.4807283a.chunk.css"
+  },
+  {
+    "revision": "8fa14d4cca435fa4cd75648728aad9dd",
+    "url": "/Statify/index.html"
+  }
+];
