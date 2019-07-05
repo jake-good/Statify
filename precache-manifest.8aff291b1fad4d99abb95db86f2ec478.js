4456989c327b2aa9082ffeb3edae3a4b310e4927
jake-good <jgoo479@aucklanduni.ac.nz>
2019-07-06 11:34:33 +1200

    Updates

---
+++ precache-manifest.8aff291b1fad4d99abb95db86f2ec478.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/Statify/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "5b4ec6f940cf16d67956",
+    "url": "/Statify/static/css/main.f514ffe0.chunk.css"
+  },
+  {
+    "revision": "87e208e86ae124bc5f6e",
+    "url": "/Statify/static/js/runtime~main.73e1833e.js"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/Statify/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "a66a1a5d713114de2d93",
+    "url": "/Statify/static/js/2.79f7f0dc.chunk.js"
+  },
+  {
+    "revision": "5b4ec6f940cf16d67956",
+    "url": "/Statify/static/js/main.54b20bbc.chunk.js"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/Statify/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/Statify/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/Statify/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "a66a1a5d713114de2d93",
+    "url": "/Statify/static/css/2.106f6b78.chunk.css"
+  },
+  {
+    "revision": "c182bb972e116508dc77fb2f9f4f50fa",
+    "url": "/Statify/index.html"
+  }
+];
