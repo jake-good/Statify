da52c940fe06dc6fde7f5f3fd896503aff76b349
jake-good <jake.good1699@gmail.com>
2023-10-31 20:00:58 +0000

    fix build script

--- client/build-config.sh
+++ client/build-config.sh
@@ -5,5 +5,5 @@ if [[ -f config.json ]] ; then rm config.json ; fi
 ## then build the file one line at a time:
 
 echo "{" >> config.json
-echo "  \"clientId\": \"${clientId}\"," >> auth_config.json
+echo "  \"clientId\": \"${clientId}\"," >> config.json
 echo "}" >> config.json
